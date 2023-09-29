def91b3f16eeafbf55e4acddefce5daa4eae8d42
stakbucks <dhassidu11@gmail.com>
2023-09-29 21:26:55 +0900

    feat: 푸터 데스크탑 크기 구현

--- anyfitness/src/components/Footer/Footer.tsx
+++ anyfitness/src/components/Footer/Footer.tsx
@@ -30,42 +30,50 @@ const contactItems = [
 
 export default function Footer() {
   return (
-    <footer className="w-screen h-auto px-[24px] py-[40px] mt-[87px] flex flex-col bg-theme-FOOTER">
-      <Image
-        alt="로고"
-        src={Logo}
-        width={162}
-        height={60}
-        className="w-[162px] h-[60px]"
-      />
-      <h2
-        className={`${monsterrat.className} mt-[24px] text-theme-B text-ENG-sm-C1`}
-      >
-        ABOUT US
-      </h2>
-      <div className="w-[50px] h-[2px] my-[16px] bg-theme-G4" />
-      <p className="text-theme-G6 text-KOR-sm-C1">{ABOUT_US_TEXT}</p>
-      <h2
-        className={`${monsterrat.className} mt-[24px] text-theme-B text-ENG-sm-C1`}
-      >
-        CONTACT US
-      </h2>
-      <div className="w-[50px] h-[2px] my-[16px] bg-theme-G4" />
-      <ul>
-        {contactItems.map(({ icon, description }, idx) => (
-          <li
-            className="flex items-center text-theme-G6 text-KOR-sm-C1 gap-[10px]"
-            key={idx}
+    <footer className=" w-screen h-auto xl:px-[40px] px-[24px] py-[40px] mt-[87px]  bg-theme-FOOTER flex xl:justify-center">
+      <div className="relative xl:flex xl:flex-row xl:justify-between flex flex-col xl:pb-[35px] xl:w-full xl:max-w-[1424px]">
+        <Image
+          alt="로고"
+          src={Logo}
+          width={162}
+          height={60}
+          className="w-[162px] h-[60px]"
+        />
+        <section className="flex xl:flex-row flex-col xl:gap-[48px]">
+          <div className="flex flex-col xl:w-[315px] pr-[10px] max-w-[600px]">
+            <h2
+              className={`${monsterrat.className} xl:mt-0 mt-[24px] text-theme-B text-ENG-sm-C1`}
+            >
+              ABOUT US
+            </h2>
+            <div className="w-[50px] h-[2px] my-[16px] bg-theme-G4" />
+            <p className="text-theme-G6 text-KOR-sm-C1">{ABOUT_US_TEXT}</p>
+          </div>
+          <div className="flex flex-col xl:w-[325px] w-auto">
+            <h2
+              className={`${monsterrat.className} xl:mt-0 mt-[24px] text-theme-B text-ENG-sm-C1`}
+            >
+              CONTACT US
+            </h2>
+            <div className="w-[50px] h-[2px] my-[16px] bg-theme-G4" />
+            <ul>
+              {contactItems.map(({ icon, description }, idx) => (
+                <li
+                  className="flex items-center text-theme-G6 text-KOR-sm-C1 gap-[10px]"
+                  key={idx}
+                >
+                  {icon} <span>{description}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <span
+            className={`${monsterrat.className} xl:absolute xl:left-0 xl:bottom-[-15px] text-theme-G4 text-ENG-sm-C2`}
           >
-            {icon} <span>{description}</span>
-          </li>
-        ))}
-      </ul>
-      <span
-        className={`${monsterrat.className} mt-[16px] text-theme-G4 text-ENG-sm-C2`}
-      >
-        {COPYRIGHT_TEXT}
-      </span>
+            {COPYRIGHT_TEXT}
+          </span>
+        </section>
+      </div>
     </footer>
   );
 }
