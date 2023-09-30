cfdc616710a0f6b1b8336a2ee6bdddf3ad6349f7
stakbucks <dhassidu11@gmail.com>
2023-09-30 18:22:34 +0900

    fix: 연혁 모바일 넓이 수정

--- anyfitness/src/components/Intro/History/History.tsx
+++ anyfitness/src/components/Intro/History/History.tsx
@@ -23,19 +23,19 @@ export default function History() {
               </p>
               <div className="flex flex-col justify-center xl:gap-[6px] gap-[4px]">
                 {details.map(({ month, description, highlight }, idx) => (
-                  <p key={idx} className="flex items-center">
-                    <span className="text-theme-G6 xl:text-KOR-lg-B5 text-KOR-sm-C5 mr-[28px] w-[24px] ">
+                  <p key={idx} className="flex items-start">
+                    <p className="text-theme-G6 xl:text-KOR-lg-B5 text-KOR-sm-C5 mr-[28px] w-[24px] ">
                       {month.toString().padStart(2, '0')}
-                    </span>
-                    <span
-                      className={`text-theme-G6 ${
+                    </p>
+                    <p
+                      className={`xl:w-auto w-[220px] truncate text-theme-G6 ${
                         highlight
                           ? 'xl:text-KOR-lg-B5 text-KOR-sm-C5'
                           : 'xl:text-KOR-lg-B1 text-KOR-sm-C1'
                       }`}
                     >
                       {description}
-                    </span>
+                    </p>
                   </p>
                 ))}
               </div>
