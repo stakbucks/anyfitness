261c68d2857d0a6869ff7af8d0736fd161e788df
stakbucks <dhassidu11@gmail.com>
2023-10-01 16:50:04 +0900

    시설현황 스키마 생성

---
+++ anyfitness-sanity/schemas/certificates.ts
@@ -0,0 +1,12 @@
+export default {
+  title: '인증서',
+  name: 'certificates',
+  type: 'document',
+  fields: [
+    {
+      title: '사진',
+      name: 'image',
+      type: 'image',
+    },
+  ],
+}
--- anyfitness-sanity/schemas/index.ts
+++ anyfitness-sanity/schemas/index.ts
@@ -2,4 +2,5 @@ import weightEq from './weightEq'
 import targetBodyParts from './targetBodyParts'
 import cardio from './cardio'
 import outdoor from './outdoor'
-export const schemaTypes = [weightEq, targetBodyParts, cardio, outdoor]
+import certificates from './certificates'
+export const schemaTypes = [weightEq, targetBodyParts, cardio, outdoor, certificates]
