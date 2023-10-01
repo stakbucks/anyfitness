3dfbb53c599b91523c8382f6c20d2ac1d78a9382
stakbucks <dhassidu11@gmail.com>
2023-10-01 16:26:35 +0900

    fix: 랜딩페이지 배너 글씨 크기 수정

--- anyfitness/src/components/Landing/Banner/Banner.tsx
+++ anyfitness/src/components/Landing/Banner/Banner.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import LandingBanner from '../../../../public/landing/landingBanner.png';
 import AnyfitnessText from '../../../../public/landing/anyfitnessLetter.svg';
 import LandingLetter from '../../../../public/landing/landingLetter.svg';
+import LetterGroup from '../../../../public/landing/letterGroup.svg';
 import DiscoverWhiteBtn from '@/UI/Buttons/DiscoverWhiteBtn';
 
 export default function Banner() {
@@ -13,19 +14,19 @@ export default function Banner() {
         fill
         className="object-cover w-[100vw] h-full z-0 "
       />
-      <Image
+      {/* <Image
         alt="anyfitnessText"
         width={169}
         height={33}
         src={AnyfitnessText}
         className="z-0"
-      />
+      /> */}
       <Image
         alt="LandingLetter"
-        width={172}
-        height={14}
-        src={LandingLetter}
-        className=" z-0 lg:w-[172px] lg:h-[14px]   mt-[15px] mb-[20px]"
+        width={258}
+        height={81}
+        src={LetterGroup}
+        className=" z-0 lg:w-[258px] lg:h-[81px] lg:scale-100 scale-75 mb-[20px]"
       />
       <DiscoverWhiteBtn to="/intro" className="z-0" />
     </div>
