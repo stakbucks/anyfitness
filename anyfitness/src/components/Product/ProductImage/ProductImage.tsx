696e77e4053e186b2c596c38890443e7dd69eb36
stakbucks <dhassidu11@gmail.com>
2023-10-16 19:27:09 +0900

    fix: 제품상세페이지 이미지 반응형 크기 수정

--- anyfitness/src/components/Product/ProductImage/ProductImage.tsx
+++ anyfitness/src/components/Product/ProductImage/ProductImage.tsx
@@ -8,12 +8,10 @@ type Props = {
 export default function ProductImage({ image }: Props) {
   return (
     <div className="relative sp:w-[994px] sp:min-h-[637px] xl:w-[498px] xl:min-h-[500px] w-full xl:h-auto h-[328px] bg-theme-IMGBG flex justify-center items-center">
-      {/* <div className="relative sp:w-[637px] sp:h-[637px] xl:w-[498px] xl:h-[498px] w-[328px] h-[328px]"></div> */}
       <Image
         alt="제품 이미지"
         src={Test}
-        fill
-        className="xl:object-cover object-contain"
+        className=" sp:h-auto xl:h-[480px] xl:object-cover object-contain h-[328px]"
       />
     </div>
   );
