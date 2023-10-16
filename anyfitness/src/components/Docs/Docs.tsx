6d20aa6d905994053c2e278e5a7650a933a40529
stakbucks <dhassidu11@gmail.com>
2023-10-16 20:46:21 +0900

    fix: 배포된 서버 url로 api 주소 변경

--- anyfitness/src/components/Docs/Docs.tsx
+++ anyfitness/src/components/Docs/Docs.tsx
@@ -1,20 +1,22 @@
 import { DocTypes, IDoc, IDocs } from '@/interface/doc';
 
+const SERVER_URI = 'https://anyfitness.vercel.app/api';
+
 export default async function Docs() {
-  //   const docs: IDocs = await fetch('http://localhost:3000/api/docs', {
-  //     cache: 'no-store',
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data: IDoc[]) => {
-  //       const result: IDocs = { catalog: [], guide: [] };
-  //       data.forEach((doc: IDoc) => {
-  //         doc.type === DocTypes.CATALOG
-  //           ? result.catalog.push(doc)
-  //           : result.guide.push(doc);
-  //       });
-  //       return result;
-  //     });
-  //   console.log(docs);
+  const docs: IDocs = await fetch(`${SERVER_URI}/docs`, {
+    cache: 'no-store',
+  })
+    .then((res) => res.json())
+    .then((data: IDoc[]) => {
+      const result: IDocs = { catalog: [], guide: [] };
+      data.forEach((doc: IDoc) => {
+        doc.type === DocTypes.CATALOG
+          ? result.catalog.push(doc)
+          : result.guide.push(doc);
+      });
+      return result;
+    });
+  console.log(docs);
   return (
     <section className="sp:w-[1424px] xl:w-[928px] w-screen xl:px-0 px-[48px]">
       <div className="w-full flex flex-col gap-[20px] mt-[40px]">
