7fd8086b075bbd202835049ea15ca0676021a28c
stakbucks <dhassidu11@gmail.com>
2023-10-16 20:05:32 +0900

    feat: 자료실 페이지 레이아웃 구현

---
+++ anyfitness/src/app/docs/page.tsx
@@ -0,0 +1,39 @@
+import Footer from '@/components/Footer/Footer';
+import LandingBanner from '../../../public/landing/landingBanner.png';
+import Image from 'next/image';
+import Path from '@/components/Paths/Path/Path';
+import Docs from '@/components/Docs/Docs';
+
+export default async function DocsPage() {
+  return (
+    <div className="w-screen h-auto lg:pt-[85px] pt-[60px] flex flex-col items-center">
+      <div className="xl:w-[1920px] lg:w-[1024px] md:w-[768px] w-[390px] lg:h-[300px] h-[240px] relative flex flex-col  justify-center items-center">
+        <Image
+          alt="배너"
+          src={LandingBanner}
+          priority
+          fill
+          className="object-cover w-[100vw] h-full z-0 "
+        />
+        <div className="relative sp:w-[1440px] xl:w-[928px] w-screen  h-full flex justify-center items-center">
+          <h2 className="text-theme-W lg:text-KOR-lg-H text-KOR-sm-H0 z-[2]">
+            자료실
+          </h2>
+          <div className="absolute xl:top-[36px] xl:top-[24px] top-[20px] xl:left-0 left-[24px] flex items-center">
+            <span
+              className={`text-theme-G3 lg:text-KOR-lg-H5 lg:text-KOR-sm-C2 `}
+            >
+              Home
+            </span>
+            <div className="lg:w-[1.5px] w-[1px] lg:h-[18px] h-[14px] bg-theme-G3 mx-[12px]" />
+            <span className="text-theme-W lg:text-KOR-lg-H4 text-KOR-sm-C4">
+              회사 소개
+            </span>
+          </div>
+        </div>
+      </div>
+      <Docs />
+      <Footer />
+    </div>
+  );
+}
---
+++ anyfitness/src/components/Docs/Docs.tsx
@@ -0,0 +1,17 @@
+export default function Docs() {
+  return (
+    <section className="sp:w-[1424px] xl:w-[928px] w-screen xl:px-0 px-[48px]">
+      <div className="w-full flex flex-col gap-[20px] mt-[40px]">
+        <h3 className="text-theme-B xl:text-KOR-lg-H1 text-KOR-sm-H3">
+          카다로그
+        </h3>
+        <div className="border-t-[2px] border-solid border-theme-B ">
+          <div className="px-[16px] py-[32px] w-full flex items-center justify-between xl:text-KOR-lg-B5">
+            <p>예시 체크중 (Example Checking)</p>
+            <a>다운로드</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
