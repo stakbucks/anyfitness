a3556f3d8bf80e657b763d21f762e357fae7750e
stakbucks <dhassidu11@gmail.com>
2023-10-20 15:00:50 +0900

    fix: 제품소개 제품 카드에서 제품명 truncate 속성 추가

--- anyfitness/src/components/Products/ProductCards/ProductCard/ProductCard.tsx
+++ anyfitness/src/components/Products/ProductCards/ProductCard/ProductCard.tsx
@@ -25,7 +25,7 @@ export default function ProductCard({ product }: Props) {
         className={`${dynamicSizes.sp} ${dynamicSizes.xl} w-auto h-[298px] px-[20px] pt-[20px] bg-theme-IMGBG flex flex-col items-center hover:scale-[1.02]`}
       >
         <div className="w-full xl:h-[34px] pb-[12px] border-b border-solid border-theme-G2">
-          <h3 className="text-theme-B xl:text-KOR-lg-H4 text-KOR-sm-B4">
+          <h3 className="text-theme-B xl:text-KOR-lg-H4 text-KOR-sm-B4 truncate">
             {name}
           </h3>
         </div>
