94a947de8fea4909f735282ff7a891b2e4e63c13
stakbucks <dhassidu11@gmail.com>
2023-10-23 21:30:07 +0900

    feat: products genereateStaticParams 적용

--- anyfitness/src/app/api/revalidate/route.ts
+++ anyfitness/src/app/api/revalidate/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-// e.g a webhook to `your-website.com/api/revalidate?tag=collection&secret=<token>`
 export async function POST(request: NextRequest) {
   revalidatePath('/products');
 
