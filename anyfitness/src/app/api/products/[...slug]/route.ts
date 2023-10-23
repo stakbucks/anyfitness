9981c373ec54412eb98a88af0908df47ddbf1cbb
stakbucks <dhassidu11@gmail.com>
2023-10-23 14:54:16 +0900

    feat: products api force dynamic 적용

--- anyfitness/src/app/api/products/[...slug]/route.ts
+++ anyfitness/src/app/api/products/[...slug]/route.ts
@@ -1,6 +1,8 @@
 import { getProductsByCategory } from '@/service/products';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 type Props = {
   params: {
     slug: [type: string, category: string];
