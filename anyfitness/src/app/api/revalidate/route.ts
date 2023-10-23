8bc6ea68891a6c1b09160059f9c2078d10097096
stakbucks <dhassidu11@gmail.com>
2023-10-23 22:15:49 +0900

    fix: revalidate 호출 시 모든 경로 revalidate 하도록 수정

--- anyfitness/src/app/api/revalidate/route.ts
+++ anyfitness/src/app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { NextRequest } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
 export async function POST(request: NextRequest) {
-  revalidatePath('/products');
+  revalidatePath('/');
 
   return Response.json({ revalidated: true, now: Date.now() });
 }
