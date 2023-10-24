ed579589e326ae067dfb0e564dfa97249aa39f28
stakbucks <dhassidu11@gmail.com>
2023-10-24 15:21:52 +0900

    fix: 자료실 콘텐츠 이름 최대 너비 수정

--- anyfitness/src/components/Docs/Doc/Doc.tsx
+++ anyfitness/src/components/Docs/Doc/Doc.tsx
@@ -9,7 +9,7 @@ export default function Doc({ doc }: Props) {
   const { url, title } = doc;
   return (
     <div className="xl:px-[16px] xl:py-[32px] px-[12px] py-[20px] w-full flex items-center justify-between xl:text-KOR-lg-B5 border-b border-solid border-theme-G2">
-      <p className="xl:text-KOR-lg-B5 text-KOR-sm-C5 xl:max-w-full max-w-[350px] truncate">
+      <p className="xl:text-KOR-lg-B5 text-KOR-sm-C5 xl:max-w-full max-w-[320px] truncate">
         {title}
       </p>
       <a
